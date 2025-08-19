89bc81ed51e0cc3ef8a0978c238cecc1e3286f1e
Christopher Kola <122616101+Ckola99@users.noreply.github.com>
2025-08-19 16:47:21 +0200

    Update Methods.java

--- src/main/java/com/test/practice/Methods.java
+++ src/main/java/com/test/practice/Methods.java
@@ -1,80 +1,78 @@
 package com.test.practice;
 
+import java.util.*;
+
 public class Methods {
 
-    // Reverse a given string
     public String reverseString(String word) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Count the number of vowels in a given string
     public int countVowels(String word) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Check if a number is prime
-    public boolean isPrime(int number) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    // Find the factorial of a number
-    public long factorial(int number) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    // Find the maximum of two numbers
-    public int max(int a, int b) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    // Check if a given string is a palindrome
-    public boolean isPalindrome(String word) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    // Count the digits of a given integer
-    public int countDigits(int number) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    // Sum of digits of a number
     public int sumDigits(int number) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Check if a number is even
-    public boolean isEven(int number) {
+    public int findMax(int[] numbers, int n) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Generate Fibonacci sequence up to n terms
-    public int[] fibonacci(int n) {
+    public boolean isPrime(int n) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Find the GCD of two numbers
-    public int gcd(int a, int b) {
+    public int gcd(int n, int m) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Find the LCM of two numbers
-    public int lcm(int a, int b) {
+    public String titleCase(String sentence) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Convert Celsius to Fahrenheit
-    public double celsiusToFahrenheit(double celsius) {
+    public List<String> fizzBuzz(int n) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Convert Fahrenheit to Celsius
-    public double fahrenheitToCelsius(double fahrenheit) {
+    public String fibonacciSequence(int n) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    // Find the minimum of two numbers
-    public int min(int a, int b) {
+    public String calculate(int a, int b, String operator) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public String drawTriangle(int n) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public boolean isPalindrome(String str) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public long factorial(int n) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public boolean uniqueChars(String word) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public boolean anagramCheck(String str1, String str2) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int[][] rotateMatrix(int[][] matrix) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int longestSubstringWithoutRepeating(String str) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
-
