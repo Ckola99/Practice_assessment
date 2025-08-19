2ddc3dce25f614f25c2514ba2fcefd3063cffa18
Christopher Kola <122616101+Ckola99@users.noreply.github.com>
2025-08-19 16:14:44 +0200

    Update Methods.java

--- src/main/java/com/test/practice/Methods.java
+++ src/main/java/com/test/practice/Methods.java
@@ -7,79 +7,79 @@ import java.util.List;
 import java.util.ArrayList;
 public class Methods {
 
-	public String reverseString(String word) {
-		// Implement
-	}
+	// public String reverseString(String word) {
+	// 	// Implement
+	// }
 
-	public int countVowels(String word) {
-		// Implement
-	}
+	// public int countVowels(String word) {
+	// 	// Implement
+	// }
 
-	public int sumDigits(int number) {
-		// Implement
-	}
+	// public int sumDigits(int number) {
+	// 	// Implement
+	// }
 
-	public int findMax(int[] numbers, int n) {
-		// Implement
-	}
+	// public int findMax(int[] numbers, int n) {
+	// 	// Implement
+	// }
 
-	public boolean isPrime(int n) {
-		// Implement
-	}
+	// public boolean isPrime(int n) {
+	// 	// Implement
+	// }
 
-	public int gcd(int n, int m) {
-		// Implement
-	}
+	// public int gcd(int n, int m) {
+	// 	// Implement
+	// }
 
-	public String titleCase(String sentence) {
-		// Implement
-	}
+	// public String titleCase(String sentence) {
+	// 	// Implement
+	// }
 
-	public  List<String> fizzBuzz(int n) {
+	// public  List<String> fizzBuzz(int n) {
 
-       // Implement
-    }
+ //       // Implement
+ //    }
 
-    public String fibonacciSequence(int n) {
-       // Implement
-    }
-
-
-    public String calculate(int a, int b, String Operator){
-        // Implement
-    }
+ //    public String fibonacciSequence(int n) {
+ //       // Implement
+ //    }
 
 
+ //    public String calculate(int a, int b, String Operator){
+ //        // Implement
+ //    }
 
-    public String drawTriangle(int n){
-        // Implement
-    }
 
-    public boolean isPalindrome(String str){
-       // Implement
-    }
 
-    public long factorial(int n) {
-        // Implement
-    }
+ //    public String drawTriangle(int n){
+ //        // Implement
+ //    }
 
-	public boolean uniqueChars(String word) {
-		// Implement
+ //    public boolean isPalindrome(String str){
+ //       // Implement
+ //    }
 
-	}
+ //    public long factorial(int n) {
+ //        // Implement
+ //    }
 
-	public boolean anagramCheck(String str1, String str2) {
-		// Implement
-	}
+	// public boolean uniqueChars(String word) {
+	// 	// Implement
 
-	public int[] twoSum(int[] nums, int target) {
-		// Implement
-	}
+	// }
 
-	public int[][] rotateMatrix(int[][] matrix) {
-		// Implement
-	}
+	// public boolean anagramCheck(String str1, String str2) {
+	// 	// Implement
+	// }
 
-	public int longestSubstringWithoutRepeating(String str) {
-		// Implement
-	}
+	// public int[] twoSum(int[] nums, int target) {
+	// 	// Implement
+	// }
+
+	// public int[][] rotateMatrix(int[][] matrix) {
+	// 	// Implement
+	// }
+
+	// public int longestSubstringWithoutRepeating(String str) {
+	// 	// Implement
+	// }
